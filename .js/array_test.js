4c6393c51d35209c7e803b45f649a8e50de8b103
장원석 <jang1suk@sungkyul.ac.kr>
2023-04-21 18:16:13 +0000

    수고했다

--- .js/array_test.js
+++ .js/array_test.js
@@ -1,4 +1,4 @@
-let empty1 = [ ]; // 빈 배열
+/*let empty1 = [ ]; // 빈 배열
 let empty2 = [, , ,]; // 쉼표 개수 = 크기
 let all = [1, "test", 3.14]; // 다른 데이터 타입 사용 가능 
 let coffe = ["americano", "latte"]; // 이름, 괄호
@@ -9,6 +9,24 @@ let car = cars[2]; // 변수에 배열 값 초기화
 cars[1] = Date.now(); // 값에 객체 삽입 가능
 
 console.log(cars); // 배열 출력
-console.log(typeof cars); // 배열 타입 : 객체
+console.log(typeof cars); // 배열 타입 : 객체*/
 
-document.getElementById("search_message").innerHTML= cars;
+var arr = ['a','b','c'];
+var arr2 = [1, 'b', 3.512];
+// arr = ['a', 'b', 'c', 'e'];
+arr[arr.length] = 'e';
+//배열의 끝에 요소를 추가
+arr = ['a', 'b', 'c'];
+arr.length = arr.length - 1;
+//배열의 크기 1개 x
+arr[5] = 'g'; //index 5에 요소를 추가
+arr2.push("pushed"); //값 추가(뒤)
+arr.shift(); //값 삭제(앞)
+arr2.unshift("pushed2"); //값 추가(앞)
+
+console.log(arr); //배열 출력
+console.log(typeof arr2[0]); //배열 출력
+console.log(arr2.toString()); //문자열로 변환 출력
+console.log(typeof arr[3]); //empty 값?
+
+//document.getElementById("search_message").innerHTML= cars;
--- .js/search.js
+++ .js/search.js
@@ -4,7 +4,7 @@ function search_message(){
 }*/
 document.getElementById("search_btn").addEventListener('click', search_message);
 
-var search_array =[]; //빈 배열 - 전역 변수
+//var search_array =[]; //빈 배열 - 전역 변수
 
 function search_message(){
    let search_str = document.getElementById("search_txt"); // 변수에 저장
@@ -14,7 +14,7 @@ function search_message(){
 	else{
 		alert("검색을 수행합니다!");
 		search_array.push(search_str.value); //배열에 검색어 추가
-		let text = document.getElementById("search_message").innerHTML = search_array.toString();// 값 변환
+		let text = document.getElementById("search_message").innerHTML = search_str.value;// 값 변환
 		document.querySelector("#form_main").submit();
 	}
 }                               
