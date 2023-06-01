7baaf1559b0a806e5e10cf854c304c7e7abc2ca5
장원석 <jang1suk@sungkyul.ac.kr>
2023-06-01 15:04:22 +0000

    웹프

--- .js/login.js
+++ .js/login.js
@@ -1,4 +1,4 @@
-function login(){
+/*function login(){
     let form = document.querySelector("#form_main");
     let id = document.querySelector("#floatingInput");
     let password = document.querySelector("#floatingPassword");
@@ -12,13 +12,14 @@ function login(){
 		session_set(); // 세션 생성
 		form.submit();
     }
-}
+}*/
 
 function logout(){
-    location.href='../index.html';
+    session_del(); // 세션 삭제
+	location.href='../index.html';
 }
 
-function get_id(){
+/*function get_id(){
     var getParameters = function(paramName){ // 변수 = 함수(이름)
     var returnValue; // 리턴값을 위한 변수 선언
     var url = location.href; // 현재 접속 중인 주소 정보 저장
@@ -34,6 +35,29 @@ function get_id(){
        } // 2중 for문 끝
 }; // 함수 끝
 alert(getParameters('id') + '님 방갑습니다!'); // 메시지 창 출력
+}*/
+
+function get_id(){
+    if(true){
+        decrypt_text();
+    }
+    else{
+        var getParameters = function(paramName){ // 변수 = 함수(이름)
+        var returnValue; // 리턴값을 위한 변수 선언
+    	var url = location.href; // 현재 접속 중인 주소 정보 저장
+    	var parameters = (url.slice(url.indexOf('?') + 1, url.length)).split('&'); // ?기준 slice 한 후 split 으로 나눔
+        	for(var i = 0; i < parameters.length; i++) { 
+          	  var varName = parameters[i].split('=')[0];
+            
+            if (varName.toUpperCase() == paramName.toUpperCase()) {
+                returnValue = parameters[i].split('=')[1];
+                return decodeURIComponent(returnValue);
+            // 나누어진 값의 비교를 통해 paramName 으로 요청된 데이터의 값만 return
+          			}
+       			} // 2중 for문 끝
+			};//함수 끝
+		alert(getParameters('id') + '님 방갑습니다!'); // 메시지 창 출력
+		}
 }
 
 function addJavascript(isname){//자바스크립트 외부 연동
@@ -43,9 +67,9 @@ function addJavascript(isname){//자바스크립트 외부 연동
 			s.setAttribute('src',jsname);
 			th.appendChild(s);
 }
-/*addJavascript('/js/security.js'); //암복호화 함수
+addJavascript('/js/security.js'); //암복호화 함수
 addJavascript('/js/session.js'); //세션 함수
-addJavascript('/js/cookie.js');*/ //쿠키 함수
+addJavascript('/js/cookie.js'); //쿠키 함수
 
 function login(){ // 로그인
     let form = document.querySelector("#form_main");
@@ -68,6 +92,7 @@ function login(){ // 로그인
     if(id.value.length === 0 || password.value.length === 0){
         alert("아이디와 비밀번호를 모두 입력해주세요.");
     }else{
+		session_set(); // 세션 생성
         form.submit();
     }
 }
@@ -115,16 +140,17 @@ function init(){ // 로그인 폼에 쿠키에서 가져온 아이디 입력
 	if(get_id) { 
 	id.value = get_id; 
 	check.checked = true;
-	session_check(); // 세션 유무 검사
     }
+	session_check(); // 세션 유무 검사
 }
 
 function session_set() { //세션 저장
     let id = document.querySelector("#floatingInput");
+	    let password = document.querySelector("#floatingPassword");
     if (sessionStorage) {
-        sessionStorage.setItem("Session_Storage_test", id.value);
-
-    } else {
+        let en_text = encrypt_text(password.value);
+        sessionStorage.setItem("Session_Storage_test", en_text);
+	} else {
         alert("로컬 스토리지 지원 x");
     }
 }
@@ -140,6 +166,52 @@ function session_get() { //세션 읽기
 function session_check() { //세션 검사
     if (sessionStorage.getItem("Session_Storage_test")) {
         alert("이미 로그인 되었습니다.");
-        location.href='index_login.html’; // 로그인된 페이지로 이동
+        location.href='index_login.html'; // 로그인된 페이지로 이동
     }
+}
+
+function session_del() {//세션 삭제
+    // Check if the sessionStorage object exists
+    if (sessionStorage) {
+        // Retrieve data
+        sessionStorage.removeItem("Session_Storage_test");
+        alert('로그아웃 버튼 클릭 확인 : 세션 스토리지를 삭제합니다.');
+    } else {
+        alert("세션 스토리지 지원 x");
+    }
+}
+
+function encodeByAES256(key, data){
+    const cipher = CryptoJS.AES.encrypt(data, CryptoJS.enc.Utf8.parse(key), {
+        iv: CryptoJS.enc.Utf8.parse(""),
+        padding: CryptoJS.pad.Pkcs7,
+        mode: CryptoJS.mode.CBC
+    });
+    return cipher.toString();
+}
+
+function decodeByAES256(key, data){
+    const cipher = CryptoJS.AES.decrypt(data, CryptoJS.enc.Utf8.parse(key), {
+        iv: CryptoJS.enc.Utf8.parse(""),
+        padding: CryptoJS.pad.Pkcs7,
+        mode: CryptoJS.mode.CBC
+    });
+    return cipher.toString(CryptoJS.enc.Utf8);
+};
+
+function encrypt_text(password){
+    const k = "key"; // 클라이언트 키
+    const rk = k.padEnd(32, " "); // AES256은 key 길이가 32
+    const b = password;
+    const eb = this.encodeByAES256(rk, b);
+    return eb;
+    console.log(eb);
+}
+
+function decrypt_text(){
+    const k = "key"; // 서버의 키
+    const rk = k.padEnd(32, " "); // AES256은 key 길이가 32
+    const eb = session_get();
+    const b = this.decodeByAES256(rk, eb);
+    console.log(b); 
 }
